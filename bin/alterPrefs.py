55d89a0587a4b9e60f0d9511355ba8eb87df6664
feimaomiao <lcpmatthew@gmail.com>
2019-12-28 18:02:51 -0800

    Enhanced preference file--almost finished!

--- bin/alterPrefs.py
+++ bin/alterPrefs.py
@@ -1,74 +1,96 @@
 from funcs import *
+import shutil, os
 class usrp():
 	def __init__(self, pref, index):
 		self.index = index
 		self.key, self.description, self.valueType, self.value, self.default, self.avaliable,  = pref
+		self.value = bool(int(self.value)) if self.valueType == 'bool' else self.value
+		self.default = bool(int(self.default)) if self.valueType == 'bool' else self.default
 
 	def __repr__(self):
-		return(str(self.description))
+		return(str(self.key))
 
-def validRequest(vartype):
-	aval = []
-	if vartype == 'bool':
-		boolvals = {True:['true','t','yes','y'], False:['false','f','no','n']}
-		aval = boolvals[True] + boolvals[False]
-		if usrinput not in aval:
-			print('This is not a valid input...')
-			waitforinput(buildColors(False))
-			return(False, None)
+def reqBool(colors):
+	usrinput = input(colors.green('Type=Bool\nPossible = ["t","true","y","yes","f","false","n","no","d", "def","default"]\nPlease enter your choice\n')).lower()
+	possible = {'t':True,'true':True,'y':True,'yes':True,'f':False,'false':False,'n':False,'no':False,'def':'def','default':'def','d':'def'}
+	if usrinput not in possible.keys():
+		emptyline()
+		print(colors.red('This is not a valid result!Please enter again!')) 
+		return reqBool(colors)
+	return possible.get(usrinput)
 
+def reqStrIL(inpLIst, colors):
+	usrinput = input(colors.green(f'Type=String in list\nPossible={inpLIst}\nPlease enter your choice\n'))
+	if usrinput not in inpLIst:
+		emptyline()
+		print(colors.red('This is not a valid result!\nPlease enter again!'))
+		return reqStrIL(inpLIst, colors)
+	return usrinput
 
+def reqLoc(colors):
+	usrinput = input(colors.green(f'Type=Location\nPossible=All\nPlease enter a location with unix-like file("~/Path/to/destination")\n'))
+	if not os.access(os.path.expanduser(usrinput), os.W_OK) or not os.path.isdir(os.path.expanduser(usrinput)):
+		emptyline()
+		print(colors.red('This is not a valid path!\nPlease try again!'))
+		return reqLoc(colors)
+	return usrinput
 
-
-# userPreferences = [alterPrefs.usrp(x, count) for count, x in enumerate(
-# 	[row for row in self.cursor.execute('SELECT * FROM userPreferences').fetchall()], start=1)]
-
-# self.cursor.executemany(
-# '''INSERT INTO userPreferences VALUES(?,?,?,?,?,?)''', 
-# # list of preferences avaliable
-# [
-
-# # System preferences in UI
-# ('verbose','Shows everything', 'bool', False, False, 'True,False'), 
-# ('copyAfterGet','Copy password after output', 'bool',True, True, 'True,False'),
-# ('askToQuit','Ask before quit','bool',False, False, 'True,False'),
-# ('customColor','Use custom color', 'bool',True, True, 'True,False'),
-# ('logLogin','Record Logins','bool',True,True, 'True,False'),
-
-# # Exports preferences
-# ('encExpDb','Export files are encrypted','bool',True, True, 'True,False'),
-# ('useDefLoc','Use default export location','bool',True, True, 'True,False'),
-# ('exportType','Export type','str in list','db','db ', 'csv,db,json,txt'),
-# ('defExpLoc','Default export location', 'string',os.path.expanduser('~/Documents'),
-#  os.path.expanduser('~/Documents'), 'True,False'),
-
-# # Backup preferences
-# ('createBcF','backup','bool',True, True, 'True,False'),
-# ('backupFileTime','Backup Passwords time','string','d','d','h,d,w,2w,m,2m,6m,y,off'),
-# ('backupLocation','The location of back-up','string',os.path.expanduser('~/Library/.pbu'), os.path.expanduser('~/Library/.pbu'), ''),
-# ('hashBackupFile','Hashing the Backup File', 'bool', True,True, 'True,False'),
-
-# # Extrasecure 
-# ('hashUserFile', 'Hash User File Name', 'bool', False,False, 'True,False'),
-# ('createRandF','Creates random nonsense files', 'bool',False, False, 'True,False')
-# ])
 class preferences():
 	def __init__(self, listOfPref):
-		self.verbose = 			[p for p in listOfPref if p.key == 'verbose'		][0]
-		self.copyAfterGet = 	[p for p in listOfPref if p.key == 'copyAfterGet'	][0]
-		self.askToQuit = 		[p for p in listOfPref if p.key == 'askToQuit'		][0]
-		self.customColor = 		[p for p in listOfPref if p.key == 'customColor'	][0]
-		self.logLogin = 		[p for p in listOfPref if p.key == 'logLogin'		][0]
-		self.encExpDb = 		[p for p in listOfPref if p.key == 'encExpDb'		][0]
-		self.useDefLoc = 		[p for p in listOfPref if p.key == 'useDefLoc'		][0]
-		self.exportType = 		[p for p in listOfPref if p.key == 'exportType'		][0]
-		self.defExpLoc = 		[p for p in listOfPref if p.key == 'defExpLoc'		][0]
-		self.createBcF = 		[p for p in listOfPref if p.key == 'createBcF'		][0]
-		self.backupFileTime = 	[p for p in listOfPref if p.key == 'backupFileTime'	][0]
-		self.backupLocation = 	[p for p in listOfPref if p.key == 'backupLocation'	][0]
-		self.hashBackupFile = 	[p for p in listOfPref if p.key == 'hashBackupFile'	][0]
-		self.hashUserFile = 	[p for p in listOfPref if p.key == 'hashUserFile'	][0]
-		self.createRandF = 		[p for p in listOfPref if p.key == 'createRandF'	][0]
+		self.preferenceDicts = {}
+		self.lisfofPref = listOfPref
+
+	def buildPrefs(self):
+		for items in self.lisfofPref:
+			key = items.key
+			value = items
+			self.preferenceDicts[key] = value
+		return None
+
+	def printCurrentPrefs(self):
+		colors = buildColors(bool(self.preferenceDicts.get('customColor').value))
+		print(colors.green('Your current preferences are as below:'))
+		print('{index:14}||{key:23}||{description:39}||{valueType:20}||{value:30}||{defaultValue:25}||{aval:29}'.format(
+			index=colors.orange('Index'), key=colors.blue('key'), description=colors.green('description'),valueType=colors.pink('value type'),
+			value=colors.lightgrey('Value'),defaultValue=colors.cyan('Default'),aval=colors.yellow('avaliable')))
+		print(colors.purple('-'*shutil.get_terminal_size().columns))
+		vertL = colors.purple('||')
+		for key, value in self.preferenceDicts.items():
+			print(f'{colors.orange(value.index):14}{vertL}{colors.blue(value.key):23}{vertL}{colors.green(value.description):39}{vertL}{colors.pink(value.valueType):20}{vertL}{colors.lightgrey(value.value):30}{vertL}{colors.cyan(value.default):25}{vertL}{colors.yellow(value.avaliable):29}')
+		print(colors.purple('-'*os.get_terminal_size().columns))			
+		return None
+
+	def changePrefs(self):
+		colors = buildColors(bool(self.preferenceDicts.get('customColor').value))
+		changeIndex = 1
+		while changeIndex != '':
+			self.printCurrentPrefs()
+			changeIndex = input(colors.yellow('Please enter the index of the prefernce you want to change:'))
+			if not changeIndex.isdigit() or not (int(changeIndex) <= len(self.preferenceDicts) and  int(changeIndex) > 0):
+				print(colors.red('ValueError:'), colors.yellow('This is not a valid input!'))
+				break
+			elif len(changeIndex) == 0:
+				break
+			changeItem = [i for k,i in self.preferenceDicts.items() if i.index == int(changeIndex)][0]
+			outputVal = bool(changeItem.value) if changeItem.valueType == 'bool' else str(changeItem.value)
+			print(colors.blue(f'You are going to change {changeItem.key}.\nThe current value is {outputVal}.\nDefault value is {changeItem.index}'))
+			if changeItem.valueType == 'bool':
+				newvalue = reqBool(colors)
+				if newvalue == 'def':
+					changeItem.value = changeItem.default
+				else:
+					changeItem.value = bool(newvalue)
+			elif changeItem.valueType == 'str in list':
+				newvalue = reqStrIL(changeItem.avaliable.split(',')+['def'], colors)
+				changeItem.value = changeItem.default if newvalue == 'def' else newvalue
+			else:
+				newvalue = reqLoc(colors)
+				changeItem.value = newvalue
+				print(changeItem.value)
+			emptyline()
+		waitForInput(colors)
+
+
 
 
 
