6413aa149bc3905771c61d94ac3405fedbc5c387
feimaomiao <lcpmatthew@gmail.com>
2020-01-03 17:10:33 -0800

    removed unused files

--- __main__.py
+++
@@ -1,4 +0,0 @@
-from exec import alterPrefs, enc, funcs, pwd, userClass
-
-if __name__ == '__main__':
-	pwd.main()
--- exec/__init__.py
+++
@@ -1,5 +0,0 @@
-from .alterPrefs import *
-from .enc import *
-from .funcs import *
-from .pwd import *
-from .userClass import *
